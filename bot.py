80dddcde388c5021b51482f6170999f68a004680
Rachit Gupta <guptarachit2004@gmail.com>
2020-06-15 00:15:14 +0530

    integrate discord.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'ha bhai ik your name is {message.author.name} and your id is {message.author.id}.')
+
+client.run(TOKEN)
